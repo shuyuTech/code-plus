13a48aadc49880e5a7810bf7b8829bd4efc22752
GitHub Action <action@github.com>
2022-06-10 15:04:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/shuyutech/codeplus/MyBundle.kt
+++ src/main/kotlin/com/github/shuyutech/codeplus/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.shuyutech.codeplus
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/shuyutech/codeplus/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/shuyutech/codeplus/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.shuyutech.codeplus.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.shuyutech.codeplus.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
